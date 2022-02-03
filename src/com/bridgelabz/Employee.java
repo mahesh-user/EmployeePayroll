18c1e946bce5c8af075840e5ce7282278a47fdc6
Mahesh <mahesh51138@gmail.com>
2022-02-03 20:27:44 +0530

    added:EmployeeWage

--- src/com/bridgelabz/Employee.java
+++ src/com/bridgelabz/Employee.java
@@ -3,7 +3,7 @@ package com.bridgelabz;
 import java.util.Random;
 
 public class Employee {
-    public  void result() {
+    public void  result() {
         Random name = new Random();
         int b = name.nextInt(2);
         int check = 0;
@@ -15,7 +15,28 @@ public class Employee {
 
         }
     }
+    public void salary() {
+        Random name = new Random();
+        int check = name.nextInt(3);
+        int WagePerHour = 20;
+        int FullDayHour = 8;
+        int PartTime = 4;
+        int TotalWage;
+        if (check == 1) {
+            System.out.println(" employee is present");
+            TotalWage = WagePerHour * FullDayHour;
+            System.out.println("employee wage is " + TotalWage);
+        } else if (check == 2) {
+            System.out.println("employee is parttime");
+            TotalWage = WagePerHour* PartTime;
+            System.out.println("employee wage is " + TotalWage);
+        } else {
+            System.out.println("employee is absent");
+            TotalWage = 0;
+            System.out.println("employee wage is " + TotalWage);
+        }
 
+
+    }
 }
-
 
--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -5,7 +5,7 @@ public class EmployeeWage {
 
     public static void main(String[] args) {
         Employee name = new Employee();
-        name.result();
+        name.salary();
 
     }
 }
