460eec4cc1b574a57303a69a2b09804732be4f89
Mahesh <mahesh51138@gmail.com>
2022-02-03 22:48:30 +0530

    added:CalucululateAsumeHours

--- src/com/bridgelabz/Employee.java
+++ src/com/bridgelabz/Employee.java
@@ -3,7 +3,7 @@ package com.bridgelabz;
 import java.util.Random;
 
 public class Employee {
-    public void  result() {
+    public void result() {
         Random name = new Random();
         int b = name.nextInt(2);
         int check = 0;
@@ -15,21 +15,26 @@ public class Employee {
 
         }
     }
+
     public void salary() {
         Random name = new Random();
         int check = name.nextInt(3);
-         final int Present=1;
-         final int Absent=0;
-         final int Fulltime=8;
-         final int Parttime=4;
-         final int WagePerHour=20;
-         final int NoOfWorkingDays = 20;
-         int DailyWage=0;
-        int monthlyWage = 0;
+        final int Present = 1;
+        final int Absent = 0;
+        final int Fulltime = 8;
+        final int Parttime = 4;
+        final int WagePerHour = 20;
+        final int NoOfWorkingDays = 20;
+        int DailyWage = 0;
+        int MonthlyWage = 0;
+        int TotalHours = 0;
+        int Hours = 0;
+        if (NoOfWorkingDays < 100 && TotalHours < 100) {
             switch (check) {
                 case Present: {
                     System.out.println("Employee is present");
                     DailyWage = Fulltime * WagePerHour;
+                    Hours = Fulltime;
                     break;
 
                 }
@@ -40,14 +45,18 @@ public class Employee {
                 default: {
                     System.out.println("Employee is part time ");
                     DailyWage = Parttime * WagePerHour;
+                    Hours = Parttime;
                     break;
                 }
+
             }
-        monthlyWage=DailyWage*NoOfWorkingDays;
-            System.out.println("Daily wage : " + DailyWage);
-        System.out.println("Monthly wage : " + monthlyWage );
+            MonthlyWage = DailyWage + MonthlyWage;
+            TotalHours = Hours + TotalHours;
+
         }
+        MonthlyWage = DailyWage * NoOfWorkingDays;
+        System.out.println("Daily wage : " + DailyWage);
+        System.out.println("Monthly wage : " + MonthlyWage);
 
     }
-
-
+}
